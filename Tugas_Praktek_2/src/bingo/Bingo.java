4440cf0ebd8b118aca8c62a513981b351ab1130d
desman012 <desmandwisaputra@gmail.com>
2024-09-11 21:49:16 +0700

    Perubahan terhadap Bingo

--- Tugas_Praktek_2/src/bingo/Bingo.java
+++ Tugas_Praktek_2/src/bingo/Bingo.java
@@ -1,43 +1,10 @@
 package bingo;
 public class Bingo {
-    int loop;
     String replace;
-    char[] bingo = {'B', 'I', 'N', 'G', 'O'};
-
-    public Bingo(int loop, String replace) {
-        this.loop = loop;
+    public Bingo(String replace) {
         this.replace = replace;
     }
-
-    // Method untuk mencetak tulisan yang berulang
-    void printTeks() {
-        System.out.println("There was a who had a dog");
-        System.out.println("And Bingo was his name-o.");
-    }
-
-    // Method untuk mencetak baris bingo dengan menggantikan beberapa huruf
-    void printBingoLine(int numReplacements) {
-        for (int i = 0; i < bingo.length; i++) {
-            if (i < numReplacements) {
-                System.out.print(replace);
-            } else {
-                System.out.print(bingo[i]);
-            }
-            if (i != bingo.length - 1) {
-                System.out.print("-");
-            }
-        }
-        System.out.println(" ");
-    }
-
-    // Method utama untuk mencetak keseluruhan teks
     void printBingo() {
-        for (int i = 0; i < 6; i++) {
-            printTeks();
-            for (int j = 0; j < loop; j++) {
-                printBingoLine(i);
-            }
-            System.out.println("And Bingo was his name-o\n");
-        }
+        System.out.println(replace);
     }
 }
--- Tugas_Praktek_2/src/bingo/BingoBeraksi.java
+++ Tugas_Praktek_2/src/bingo/BingoBeraksi.java
@@ -2,7 +2,24 @@ package bingo;
 public class BingoBeraksi {
     public static void main(String[] args) {
         // TODO code application logic here
-        Bingo bg = new Bingo(3,"(clap)");
+        String lirik = "There was a farmer who had a dog\nAnd Bingo was his name-o.\n"
+            +"B-I-N-G-O\nB-I-N-G-O\nB-I-N-G-O\nAnd Bingo was his name-o\n\n"
+            +"There was a farmer who had a dog\nAnd Bingo was his name-o.\n"
+            +"(clap)-I-N-G-O\n(clap)-I-N-G-O\n(clap)-I-N-G-O\n"
+            +"And Bingo was his name-o\n\n"
+            +"There was a farmer who had a dog\nAnd Bingo was his name-o.\n"
+            +"(clap)-(clap)-N-G-O\n(clap)-(clap)-N-G-O\n(clap)-(clap)-N-G-O\n"
+            +"And Bingo was his name-o\n\n"
+            +"There was a farmer who had a dog\nAnd Bingo was his name-o.\n"
+            +"(clap)-(clap)-(clap)-G-O\n(clap)-(clap)-(clap)-G-O\n(clap)-(clap)-(clap)-G-O\n"
+            +"And Bingo was his name-o\n\n"
+            +"There was a farmer who had a dog\nAnd Bingo was his name-o.\n"
+            +"(clap)-(clap)-(clap)-(clap)-O\n(clap)-(clap)-(clap)-(clap)-O\n(clap)-(clap)-(clap)-(clap)-O\n"
+            +"And Bingo was his name-o\n\n"
+            +"There was a farmer who had a dog\nAnd Bingo was his name-o.\n"
+            +"(clap)-(clap)-(clap)-(clap)-(clap)\n(clap)-(clap)-(clap)-(clap)-(clap)\n(clap)-(clap)-(clap)-(clap)-(clap)\n"
+            +"And Bingo was his name-o\n\n";
+        Bingo bg = new Bingo(lirik);
         bg.printBingo();
     }  
 }
